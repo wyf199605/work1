1605efa0283bf68d517a6a6008171d040ca6e9a5
吴梦龙 <wuml@fastlion.cn>
2018-10-22 18:02:38 +0800

    流程引擎bug修改，移动化详情页

--- app/global/test.tsx
+++ app/global/test.tsx
@@ -1,22 +1,9 @@
 /// <amd-module name="GlobalTestModule"/>
 
-import {MbList} from "./components/mbList/MbList";
+import {Uploader} from "./components/form/upload/uploader";
 
-new MbList({
-    data: [
-        {
-            title: '9825445464456 驼色',
-            body: [
-                ['码数', '张三'],
-                ['库存量', '2016-8-23 08:00'],
-                ['异店库存','2016-8-23 08:00']
-            ],
-            label: ['手工确认完成', '谢晋超'],
-            imgLabel: '标签',
-            status: 1,
-        }
-    ],
-    isMulti: true
+new Uploader({
+    uploadUrl:'',
+    nameField:'',
+    container:document.body
 });
-
-
