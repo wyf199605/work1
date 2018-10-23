ca5302f13e6dd1a25ab9d25e8c891d65eefe343e
吴晓华 <wuxh@fastlion.cn>
2018-10-23 15:55:00 +0800

    no message

--- app/global/test.tsx
+++ app/global/test.tsx
@@ -1,23 +1,15 @@
 /// <amd-module name="GlobalTestModule"/>
 
-import {MbList} from "./components/mbList/MbList";
 
-new MbList({
+import {UploadFileList} from "./components/uploadFile/UploadFileList";
+
+new UploadFileList({
     data: [
         {
-            title: '9825445464456 驼色',
-            body: [
-                ['码数', '张三'],
-                ['库存量', '2016-8-23 08:00'],
-                ['异店库存','2016-8-23 08:00']
-            ],
-            label: ['手工确认完成', '谢晋超'],
-            imgLabel: '标签',
-            btns: ['确认核验', '加入色码打印', '测试'],
-            status: 1,
+            uploadUrl: 'test',
+            fileInfo: ['1.pdf', '123M']
         }
-    ],
-    isMulti: true
+    ]
 });
 
 
