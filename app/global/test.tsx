ad79ec2e30bdc239a29a896bfd87403ba0050675
吴晓华 <wuxh@fastlion.cn>
2018-10-24 17:34:35 +0800

    又一次修改mbList样式

--- app/global/test.tsx
+++ app/global/test.tsx
@@ -1,5 +1,20 @@
 /// <amd-module name="GlobalTestModule"/>
-import {Accessory} from "./components/form/upload/accessory";
-new Accessory({
-    container:document.body
+
+import {MbList} from "./components/mbList/MbList";
+
+new MbList({
+    data: [
+        {
+            title: '9825445464456 驼色',
+            body: [
+                ['码数', '张三'],
+                ['库存量', '2016-8-23 08:00'],
+                ['异店库存', '2016-8-23 08:00']
+            ],
+            label: ['手工确认完成', '谢晋超'],
+            imgLabel: '标签',
+            status: 1,
+        }
+    ],
+    isMulti: true
 });
