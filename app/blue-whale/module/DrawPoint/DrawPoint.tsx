55b37135fa0434c97d3976ec91ff0297dbb14854
林宇淇 <linyq@fastlion.cn>
2018-11-05 15:18:24 +0800

    描点绘图

--- app/blue-whale/module/DrawPoint/DrawPoint.tsx
+++ app/blue-whale/module/DrawPoint/DrawPoint.tsx
@@ -345,6 +345,7 @@ export class DrawPoint extends Component {
         update.each(function (d) {
             updateStr.push(d);
         })
+
         return {
             insert: [{}, {}],
             update: updateStr,
@@ -445,7 +446,7 @@ export class DrawPoint extends Component {
             let path = sl.select('path').attr('id'),
                 i = parseInt(path.slice(4, path.length));
 
-            newDelivery['point'] = this.map.get(i);
+            newDelivery[DrawPoint.POINT_FIELD] = JSON.stringify(this.map.get(i));
             sl.datum(newDelivery);
 
         } else {
@@ -472,7 +473,8 @@ export class DrawPoint extends Component {
             let path = sl.select('path').attr('id'),
                 i = parseInt(path.slice(4, path.length));
 
-            delivery['point'] = this.map.get(i);
+            delivery[DrawPoint.POINT_FIELD] = JSON.stringify(this.map.get(i));
+
             sl.attr('class', function (d) {
                 debugger
                 let num = 0;
