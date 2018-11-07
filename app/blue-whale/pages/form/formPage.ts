85f7993cb41699da469a0064bf6eb37a3bd4b0ab
吴梦龙 <wuml@fastlion.cn>
2018-11-07 13:03:00 +0800

    权限树 收件箱上传控件适配

--- app/blue-whale/pages/form/formPage.ts
+++ app/blue-whale/pages/form/formPage.ts
@@ -17,6 +17,7 @@ export = class FormPage extends BasicPage {
         super(para);
         let emPara: EditModulePara = {fields : []};
         let nameFields : {[name : string] : R_Field} = {};
+
         para.fm.fields.forEach(function (f) {
             nameFields[f.name] = f;
 
--- app/blue-whale/pages/permissionTree/permissionTreePage.tsx
+++ app/blue-whale/pages/permissionTree/permissionTreePage.tsx
@@ -5,10 +5,15 @@ import {ITablePagePara} from "../table/newTablePage";
 import {PermissionTree} from "../../../global/components/navigation/permissionTree/permissionTree";
 import tools = G.tools;
 import {BwRule} from "../../common/rule/BwRule";
+import {InputBox} from "../../../global/components/general/inputBox/InputBox";
+import {Button} from "../../../global/components/general/button/Button";
+import {ButtonAction} from "../../common/rule/ButtonAction/ButtonAction";
+
 export class PermissionTreePage extends BasicPage {
     private permissionTree: PermissionTree;
-    private buttonsWrapper:HTMLElement;
-    private treeWrapper:HTMLElement;
+    private buttonsWrapper: HTMLElement;
+    private treeWrapper: HTMLElement;
+
     constructor(private para: ITablePagePara) {
         super(para);
         this.buttonsWrapper = <div className="permission-page-buttons"/>;
@@ -19,18 +24,33 @@ export class PermissionTreePage extends BasicPage {
         this.initPermissionTree();
     }
 
-    private initPermissionTree(){
+    private initPermissionTree() {
         let dataAddr = this.para.ui.body.elements[0].dataAddr;
-        tools.isNotEmpty(dataAddr) && BwRule.Ajax.fetch(BW.CONF.siteUrl + BwRule.reqAddr(dataAddr)).then(({response})=>{
+        tools.isNotEmpty(dataAddr) && BwRule.Ajax.fetch(BW.CONF.siteUrl + BwRule.reqAddr(dataAddr)).then(({response}) => {
             this.permissionTree = new PermissionTree({
-                treeData:response.data,
-                container:this.treeWrapper
+                treeData: response.data,
+                container: this.treeWrapper
             })
         });
     }
 
     private initButtons() {
-
+        let subButtons = this.para.ui.body.elements[0].subButtons || [];
+        if (tools.isNotEmptyArray(subButtons)){
+            let inputBox = new InputBox({
+                container:this.buttonsWrapper
+            });
+            subButtons.forEach((button) => {
+                let btn = new Button({
+                    content:button.caption || button.title,
+                    icon:button.icon,
+                    onClick:()=>{
+                        ButtonAction.get().clickHandle(button,this.getData());
+                    }
+                });
+                inputBox.addItem(btn);
+            })
+        }
     }
 
     private getData() {
