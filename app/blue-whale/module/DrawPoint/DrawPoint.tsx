0d911d1f7356a33329a4d59b1124f3756e996ca3
林宇淇 <linyq@fastlion.cn>
2018-11-07 15:12:44 +0800

    描点绘图

--- app/blue-whale/module/DrawPoint/DrawPoint.tsx
+++ app/blue-whale/module/DrawPoint/DrawPoint.tsx
@@ -205,8 +205,9 @@ export class DrawPoint extends Component {
                             this.map.set(index, data.data)
                             return this.line(data.data)
                         })
-                } else {
+                } else if(data.isShow){
                     //绘字
+
                     let text = group.append('text').datum(data.name)
                         .attr('fill', 'black')
                         .attr('font-size', '14px')
@@ -496,7 +497,7 @@ export class DrawPoint extends Component {
         if (sl.selectAll('text').empty()) {
             let newDelivery = {}
             this.format(data).forEach((anl, I) => {
-                if (!anl.isPoint) {
+                if (!anl.isPoint && anl.isShow) {
                     let text = sl.append('text').datum(anl.name)
                         .attr('fill', 'black')
                         .attr('font-size', '14px')
