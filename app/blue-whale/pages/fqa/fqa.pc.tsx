3431f4b623da5dfd95a844b688bc268ce6decfac
xiaofa <wuxh@fastlion.cn>
2018-11-07 15:06:17 +0800

    修改 fqa 样式覆盖的问题

--- app/blue-whale/pages/fqa/fqa.pc.tsx
+++ app/blue-whale/pages/fqa/fqa.pc.tsx
@@ -20,6 +20,7 @@ export class FqaPcModal{
             header:'常见问题',
         });
         this.modal.className = 'full-screen-fixed';
+        this.modal.className = 'fqa-contain-modal';
     }
     static initPanel(el: HTMLElement) {
         d.append(el,FqaPcModal.initDownloadTap());
