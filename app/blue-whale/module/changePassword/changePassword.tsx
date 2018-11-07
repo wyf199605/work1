6a5b6955332ec76f95bf62eba9dda135582731f5
xiaofa <wuxh@fastlion.cn>
2018-11-07 15:14:16 +0800

    调整 changePassword的代码

--- app/blue-whale/module/changePassword/changePassword.tsx
+++ app/blue-whale/module/changePassword/changePassword.tsx
@@ -6,24 +6,24 @@ import {Button} from "../../../global/components/general/button/Button";
 
 export interface IChangePasswordPara{
     container?:HTMLElement;
-    confirm?: (obj) => void ;
+    confirm?: (obj) => Promise<any>;
 }
 
 export class ChangePassword{
    protected modal:Modal;
    constructor(para:IChangePasswordPara){
-       let body=<div className="cpw-content"/>;
+       let body=<div class="cpw-content">
+           {ChangePassword.initInput()}
+       </div>;
        let confirm = new Button({
            content:'确定',
            className:'btn-confirm',
            onClick:() =>{
                if(this.validate(body)){
                    let data = this.dataGet(body);
-                   if(tools.isFunction( para.confirm )){
-                       para.confirm(data);
+                   tools.isFunction(para.confirm) && para.confirm(data).then(()=>{
                        this.modal.isShow = false;
-                       this.destory();
-                   }
+                   })
                }
            }
        });
@@ -31,14 +31,12 @@ export class ChangePassword{
            content:'取消',
            className:'btn-cancel',
            onClick:() =>{
-                       this.modal.isShow = false;
-                       this.destory();
+               this.modal.isShow = false;
            }
        });
        let footer = {
            rightPanel:[confirm,cancel]
        };
-       ChangePassword.initPanel(body);
        this.modal = new Modal({
            container: para.container,
            body,
@@ -52,53 +50,47 @@ export class ChangePassword{
 
    }
    private validate(el:HTMLElement){
-       let oldPassword = d.query('#old-password', el) as HTMLInputElement;
-       let newPassword = d.query('#new-password', el) as HTMLInputElement;
-       let confirmPassword = d.query('#confirm-password', el) as HTMLInputElement;
+       let oldPassword = d.query('.old-password', el) as HTMLInputElement;
+       let newPassword = d.query('.new-password', el) as HTMLInputElement;
+       let confirmPassword = d.query('.confirm-password', el) as HTMLInputElement;
        if(oldPassword.value == '' || newPassword.value == '' || confirmPassword.value == ''){
            alert('您有未填写的项目');
            return false;
        }else{
            if( confirmPassword.value !== newPassword.value){
                alert('两次输入的密码不一致');
-
                return false;
            }
        }
        return true;
    }
    private dataGet(el:HTMLElement){
-       let oldPassword = d.query('#old-password', el) as HTMLInputElement;
-       let newPassword = d.query('#new-password', el) as HTMLInputElement;
-       let confirmPassword = d.query('#confirm-password', el) as HTMLInputElement;
+       let oldPassword = d.query('.old-password', el) as HTMLInputElement;
+       let newPassword = d.query('.new-password', el) as HTMLInputElement;
+       let confirmPassword = d.query('.confirm-password', el) as HTMLInputElement;
        let data = [];
        data['old'] = oldPassword.value;
        data['new'] = newPassword.value;
        data['confirm'] = confirmPassword.value;
        return data;
    }
-   static initPanel(el: HTMLElement){
-       d.append(el,ChangePassword.initInput());
-   }
    static initInput(){
        return <form action="#" class="password-form">
            <div class="form-group">
-               <span>旧密码：</span><input id="old-password" type="password" placeholder="请输入旧密码"/>
+               <span>旧密码：</span><input class="old-password" type="password" placeholder="请输入旧密码"/>
            </div>
            <div class="form-group">
-               <span>新密码：</span><input id="new-password" type="password" placeholder="请输入新密码"/>
+               <span>新密码：</span><input class="new-password" type="password" placeholder="请输入新密码"/>
            </div>
            <div class="form-group">
-               <span>确认密码：</span><input id="confirm-password" type="password" placeholder="确认新密码"/>
+               <span>确认密码：</span><input class="confirm-password" type="password" placeholder="确认新密码"/>
            </div>
            <div class="btn-group"></div>
        </form>
    }
    destory(){
        document.body.classList.remove('cpw-content');
+       this.modal && this.modal.destroy();
        this.modal = null;
    }
 }
-
-
-
