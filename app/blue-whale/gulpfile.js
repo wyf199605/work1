6cda24361e06691e74e7ca1f7187bce1699cbce4
翁冰峰 <wengbf@fastlion.cn>
2018-11-19 17:39:12 +0800

    删除keyStep合并到inputs中

--- app/blue-whale/gulpfile.js
+++ app/blue-whale/gulpfile.js
@@ -255,8 +255,6 @@ gulp.task('js', function () {
     // gulpTsModule('newStatistic/basic', 'basic.js');
     gulpTsModule('menuMrg/menuMrg', 'menu.mrg.js');
 
-
-    gulpTsModule('keyStep/KeyStep', 'KeyStep.js');
     gulpTsModule('mobileScan/MobileScan', 'MobileScan.js');
     gulpTsModule('mbListModule/mbListModule','MbListModule.js');
 
