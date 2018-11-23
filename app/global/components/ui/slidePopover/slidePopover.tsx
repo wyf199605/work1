639eeadd0a0cbf59758b023b2a882e340bff5982
吴梦龙 <wuml@fastlion.cn>
2018-11-23 16:09:11 +0800

    修复点击按钮子表不刷新 移动化列表按钮组

--- app/global/components/ui/slidePopover/slidePopover.tsx
+++ app/global/components/ui/slidePopover/slidePopover.tsx
@@ -5,11 +5,13 @@ import tools = G.tools;
 import Component = G.Component;
 import IComponentPara = G.IComponentPara;
 import {Button, IButton} from "../../general/button/Button";
-import {Modal, IModal} from "../../feedback/modal/Modal";
+
+interface ISlidePopoverModal extends IComponentPara {
+    buttons?: IButton[];
+}
 
 export interface ISlidePopoverPara extends IComponentPara {
     buttons?: IButton[];
-    modal?: IModal;
 }
 
 export class SlidePopover extends Component {
@@ -21,16 +23,6 @@ export class SlidePopover extends Component {
         </div>;
     }
 
-    // 弹出的模态框
-    private _modal: Modal = null;
-    get modal() {
-        return this._modal;
-    }
-
-    set modal(modal: Modal) {
-        this._modal = modal;
-    }
-
     // 所有按钮
     private _buttons: IButton[] = [];
     get buttons() {
@@ -39,80 +31,42 @@ export class SlidePopover extends Component {
 
     set buttons(buttons: IButton[]) {
         this._buttons = buttons;
+        if (tools.isNotEmpty(buttons)) {
+            this.createButtons();
+            this.modal = new SlidePopoverModal({
+                container: this.wrapper,
+                buttons: buttons
+            });
+        }
     }
-
-    private paraModal:IModal = {};
+    modal: SlidePopoverModal;
     constructor(para: ISlidePopoverPara) {
         super(para);
-        if (tools.isNotEmpty(para.buttons)) {
-            this.buttons = para.buttons;
-            this.createButtons();
-            this.createModal();
-        }
-        this.paraModal = para.modal || {};
+        this.buttons = para.buttons || [];
         this.initEvent.on();
     }
 
     // 创建按钮组
     private createButtons() {
+        let wrapper = d.query('.slide-buttons', this.wrapper);
         this.buttons.forEach((button) => {
-            let slideButtonWrapper = <div className="slide-button-wrapper">
-                <Button {...Object.assign(button, {size: 'large'})}/>
-            </div>;
-            d.append(d.query('.slide-buttons', this.wrapper), slideButtonWrapper);
+            new Button(Object.assign(button, {size: 'large',container:wrapper}));
         });
-    }
-
-    // 创建模态框
-    private createModal() {
-        let buttonsWrapper = <div className="buttons-wrapper"/>;
-        this.buttons.forEach(button => {
-            buttonsWrapper.appendChild(<div className="btn-wrapper"><Button {...Object.assign({},button,{size:'small'})}/></div>);
-        });
-        this.modal = new Modal(Object.assign({
-            width: '100%',
-            isShow: false,
-            isBackground: true,
-            position: 'up',
-            container: this.wrapper,
-            body:buttonsWrapper
-        }, this.paraModal));
-        let rows = this.buttons.length / 3;
-        this.modal.height = 42 * rows + 10 * 2 + 'px';  // 10*2是指上下的padding
     }
 
     // 初始化事件
     private initEvent = (() => {
-        let toggleClickHandler = () => {
-                // 显示模态框
-                this.modal.isShow = true;
-            },
-            buttonClickHandler = (e) => {
-                // 点击按钮改变对应边框的颜色
-                d.queryAll('button.selected', this.wrapper).forEach(button => {
-                    button.classList.remove('selected');
-                });
-                let buttonWrapper = d.closest(e.target.parentElement, 'button', this.wrapper);
-                buttonWrapper && buttonWrapper.classList.add('selected');
-            },
-            modalButtonsClickHandler = (e) => {
-                // 点击模态框的按钮改变对应边框的颜色
-                d.queryAll('button.selected', d.query('.modal-body')).forEach(button => {
-                    button.classList.remove('selected');
-                });
-                let buttonWrapper = d.closest(e.target.parentElement, 'button', this.wrapper);
-                buttonWrapper && buttonWrapper.classList.add('selected');
-            };
+        let toggleClickHandler = (e) => {
+            // 显示模态框
+            e.stopPropagation();
+            this.modal.isShow = true;
+        };
         return {
             on: () => {
                 d.on(this.wrapper, 'click', '.popover-toggle', toggleClickHandler);
-                this.wrapper.addEventListener('click', buttonClickHandler, true);
-                this.modal.bodyWrapper.addEventListener('click', modalButtonsClickHandler, true);
             },
             off: () => {
                 d.off(this.wrapper, 'click', '.popover-toggle', toggleClickHandler);
-                this.wrapper.removeEventListener('click', buttonClickHandler, true);
-                this.modal.bodyWrapper.removeEventListener('click', modalButtonsClickHandler, true);
             }
         }
     })();
@@ -121,6 +75,73 @@ export class SlidePopover extends Component {
     destroy() {
         this.initEvent.off();
         this.modal.destroy();
+        super.destroy();
+    }
+}
+
+class SlidePopoverModal extends Component {
+    private buttonsWrapper: HTMLElement;
+
+    protected wrapperInit(para: ISlidePopoverModal): HTMLElement {
+        return <div className="slider-popover-modal-wrapper">
+            <div className="slider-title">
+                <span>选择按钮</span>
+                <div className="slider-popover-modal-toggle iconfont icon-arrow-up"/>
+            </div>
+            {this.buttonsWrapper = <div className="buttons-wrapper"/>}
+        </div>;
+    }
+
+    constructor(para: ISlidePopoverModal) {
+        super(para);
+        this.createButtons(para.buttons || []);
+        this.initEvent.on();
+    }
+
+    private createButtons(buttons: IButton[]) {
+        buttons.forEach(button => {
+            this.buttonsWrapper.appendChild(<div className="btn-wrapper">
+                <Button {...Object.assign({}, button, {size: 'small'})}/></div>);
+        });
+    }
+
+    private _isShow: boolean;
+    set isShow(isShow: boolean) {
+        this._isShow = isShow;
+        if (isShow) {
+            this.wrapper.style.display = 'block';
+            this.wrapper.classList.remove('slider-popover-animation-out');
+            this.wrapper.classList.add('slider-popover-animation-in');
+        } else {
+            this.wrapper.classList.remove('slider-popover-animation-in');
+            this.wrapper.classList.add('slider-popover-animation-out');
+            setTimeout(()=>{
+                this.wrapper.style.display = 'none';
+            },300);
+        }
+    }
+
+    get isShow() {
+        return this._isShow;
+    }
+
+    private initEvent = (() => {
+        let toggleClickHandler = (e) => {
+            e.stopPropagation();
+            this.isShow = false;
+        };
+        return {
+            on: () => {
+                d.on(this.wrapper, 'click', '.slider-popover-modal-toggle', toggleClickHandler);
+            },
+            off: () => {
+                d.off(this.wrapper, 'click', '.slider-popover-modal-toggle', toggleClickHandler);
+            }
+        }
+    })();
+
+    destroy() {
+        this.initEvent.off();
         super.destroy();
     }
 }
