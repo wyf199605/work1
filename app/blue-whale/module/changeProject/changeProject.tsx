e1404dce1490eabfff25f060c49cc540601a521e
翁一帆 <wengyf@fastlion.cn>
2019-01-10 10:27:12 +0800

    项目切换

--- app/blue-whale/module/changeProject/changeProject.tsx
+++ app/blue-whale/module/changeProject/changeProject.tsx
@@ -3,8 +3,11 @@
 import d = G.d;
 import tools = G.tools;
 import {IModal, Modal} from "../../../global/components/feedback/modal/Modal";
-import {DropDown} from "../../../global/components/ui/dropdown/dropdown";
+import {DropDown, ListData} from "../../../global/components/ui/dropdown/dropdown";
 import {BwRule} from "../../common/rule/BwRule";
+import {SelectInputMb} from "../../../global/components/form/selectInput/selectInput.mb";
+import {SelectInput} from "../../../global/components/form/selectInput/selectInput";
+import {TextInput} from "../../../global/components/form/text/text";
 
 export interface IChangeProjectPara extends IModal {   // 类构造函数的参数
     current: string;    // 当前项目
@@ -15,27 +18,19 @@ interface IResponseProjectItem {   // 后台响应的数据
     PLATFORM_NAME: string;
     CAPTION: string;
 }
-interface IProjectListItem extends ListItem {   // 列表项的数据类型
-    caption?: string;
-}
-interface IProjectListData extends Array<IProjectListItem>{ // 传递给Dropdown的项目数据
-
-}
 
 export class ChangeProject extends Modal {
 
-    private dropdown: DropDown = null;  // 下拉列表
+    protected select: SelectInput | SelectInputMb;
     private showProject: HTMLInputElement = null;   // 显示当前选择的项目
-    private projectList: IProjectListData = null;   // 项目列表
+    private projectList:  Array<ListItem> = null;   // 项目列表
 
     constructor(para?: IChangeProjectPara) {
         super(Object.assign({
             header: {
                 title: '切换项目'
             },
-            footer: {
-
-            },
+            footer: {},
             className: 'modal-change-project',
             width: tools.isMb ? '100%' : '25%',
             height: tools.isMb && '100%',
@@ -47,97 +42,83 @@ export class ChangeProject extends Modal {
     }
 
     private initBody = () => {
+        let SelectClass: typeof SelectInputMb | typeof SelectInput
+            = tools.isMb ? SelectInputMb : SelectInput,
+            textInput: TextInput;
         let content = <div className="change-project">
             <div className="current-project-wrapper">
                 <span className="description">当前：</span>
-                <span type="text" id="current-project" className="current-project">
-                    {this.current}
-                </span>
+                {textInput = <TextInput readonly={true}/>}
             </div>
             <div className="project-list-wrapper">
                 <div className="select-project">
                     <span className="description">切换：</span>
-                    <div className="show-project-wrapper">
-                        <input type="text" className="show-project" readOnly/>
-                        <i className="appcommon app-xiala"/>
-                    </div>
+                    {this.select = <SelectClass readonly={true} clickType={0} ajax={{
+                        url: BW.CONF.ajaxUrl.projectList,
+                        fun: (url, value, callback) => {
+                            BwRule.Ajax.fetch(BW.CONF.ajaxUrl.projectList, {
+                                type: 'GET',
+                            }).then(({response}) => {
+                                // console.log('in getProjectList: ');
+                                // console.log(response);
+                                this.projectList = response.data.map((obj: IResponseProjectItem) =>
+                                    ({
+                                        value: obj.PLATFORM_SEQ,
+                                        text: obj.PLATFORM_NAME
+                                    }));
+                                callback(this.projectList);
+                                this.initEvent.on();
+                            }).catch(err => {
+                                console.log(err);
+                            });
+                        }
+                    }}/>}
                 </div>
             </div>
         </div>;
+        textInput.value = this.current
         this.showProject = d.query('input.show-project', content) as HTMLInputElement;
-        BwRule.Ajax.fetch(BW.CONF.ajaxUrl.projectList, {
-            type: 'GET',
-        }).then(({response}) => {
-            // console.log('in getProjectList: ');
-            // console.log(response);
-            this.projectList = response.data.map((obj: IResponseProjectItem)  =>
-                ({value: parseInt(obj.PLATFORM_SEQ), text: obj.PLATFORM_NAME, caption: obj.CAPTION || ''}));
-            this.dropdown = new DropDown({
-                el: d.query('.project-list-wrapper', content),
-                data: this.projectList,
-                inline: true,
-                onSelect: (item: IProjectListItem)=> {
-                    // item.caption = 'cms3';
-                    this.showProject.value = (tools.isNotEmpty(item.caption) ? item.caption + '  ' : '') + item.text;
-                    this.dropdown.hideList();
-                }
-            });
-            this.dropdown.hideList();
-            d.append(this.bodyWrapper, content);
-            this.initStyle();
-            this.initEvent.on();
-        }).catch(err => {
-            console.log(err);
-        });
-    };
-
-    private initStyle = () => {
-        // 设置样式
-        d.query('.appcommon.app-xiala', this.wrapper) &&
-            (d.query('.appcommon.app-xiala', this.wrapper).style.lineHeight = window.getComputedStyle(this.showProject).height);
-
-        if(tools.isMb){
-            d.query('.current-project', this.wrapper).style.lineHeight = window.getComputedStyle(this.showProject).height;
-            this.dropdown.getUlDom().style.left = (d.query('.current-project', this.wrapper).offsetLeft - 18)+ 'px';
-        }else {
-            this.dropdown.getUlDom().style.width = window.getComputedStyle(this.showProject).width;
-            this.dropdown.getUlDom().style.left = d.query('.show-project-wrapper', this.wrapper).offsetLeft + 'px';
-        }
+        d.append(this.bodyWrapper, content);
     };
 
     private initEvent = (() => {
-        let toggleClickHandler = () => {
-            this.dropdown.toggle();
-        };
 
         return {
             on: () => {
-                d.on(d.query('.appcommon.app-xiala', this.bodyWrapper), 'click', toggleClickHandler);
                 this.onOk = () => {
-                    if(this.dropdown.selectedIndex >= 0){
-                        let selectItem = this.projectList[this.dropdown.selectedIndex];
-                        BwRule.Ajax.fetch(BW.CONF.ajaxUrl.projectList, {
-                            type: 'PUT',
-                            data: {
-                                platform_seq: selectItem['value'],
-                                platform_name: selectItem['text'],
-                            }
-                        }).then(({response}) => {
-                            let prevUserInfo = JSON.parse(localStorage.getItem('userInfo'));
-                            localStorage.setItem('userInfo', JSON.stringify({...prevUserInfo, platformName: this.showProject.value}));
-                            Modal.toast(response.msg);
-                            location.reload();
-                        }).catch(err => {
-                            console.log(err);
-                        });
+                    let value = this.select.get(),
+                        ajaxData: obj = {};
+                    for(let item of this.projectList){
+                        if(item.value === value){
+                            ajaxData = {
+                                platform_seq: item.value,
+                                platform_name: item.text,
+                            };
+                            break;
+                        }
                     }
+                    console.log(ajaxData);
+                    BwRule.Ajax.fetch(BW.CONF.ajaxUrl.projectList, {
+                        type: 'PUT',
+                        data: ajaxData
+                    }).then(({response}) => {
+                        let prevUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+                        localStorage.setItem('userInfo', JSON.stringify({
+                            ...prevUserInfo,
+                            platformName: this.showProject.value
+                        }));
+                        Modal.toast(response.msg);
+                        location.reload();
+                    }).catch(err => {
+                        console.log(err);
+                    });
+
                     this.destroy();
                 };
                 this.onCancel = () => this.destroy();
                 this.onClose = () => this.destroy();
             },
             off: () => {
-                d.off(d.query('.appcommon.app-xiala', this.bodyWrapper), 'click', toggleClickHandler);
                 this.onOk = null;
                 this.onCancel = null;
                 this.onClose = null;
@@ -155,7 +136,7 @@ export class ChangeProject extends Modal {
     }
 
     destroy() {
-        this.dropdown.destroy();
+        // this.dropdown.destroy();
         this.initEvent.off();
         super.destroy();
     }
