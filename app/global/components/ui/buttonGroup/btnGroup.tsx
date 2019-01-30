151a0f6f62694ac233f2b95c4ba894142ff99a4d
wangjb <wangjb@fastlion.cn>
2019-01-30 18:22:14 +0800

    修改btnGroup组件格式化空格

--- app/global/components/ui/buttonGroup/btnGroup.tsx
+++ app/global/components/ui/buttonGroup/btnGroup.tsx
@@ -13,17 +13,14 @@ export class BtnGroup extends Component {
     private dropDown: DropDown;
     constructor(para: IBtnGroupPara) {
         super(para);
-        console.log(para)
         this._buttons = para.buttons;
         this.maxBtn = para.maxBtn ? para.maxBtn : 999;
     }
-
     private _buttons: IButton[];
     private _maxBtn: number;
     get maxBtn() {
         return this._maxBtn;
     }
-
     set maxBtn(maxBtn: number) {
         this.wrapper.innerHTML = ''
         let len = this._buttons.length;
@@ -34,19 +31,13 @@ export class BtnGroup extends Component {
                 }))
             }
         });
-
         if (len > maxBtn) {
             let moreBtn = this._buttons.slice(maxBtn, len);
-            let data = moreBtn.map(btn => {
+            let data = moreBtn.map(item => {
                 return {
-                    title: btn.content,
-                    click: btn.onClick
+                    text: item.content, click: item.onClick
                 }
             });
-            let redundantList: ListItem[] = [];
-            moreBtn.forEach(item => {
-                redundantList.push({ text: item.content, click: item.onClick })
-            })
             let more = new Button(Object.assign({ content: "更多" }, {
                 container: this.wrapper,
                 onClick: () => {
@@ -59,7 +50,7 @@ export class BtnGroup extends Component {
                                 let click = item.click;
                                 typeof click === 'function' && click();
                             },
-                            data: redundantList,
+                            data: data,
                             multi: false,
                             className: "more-btn"
                         });
@@ -69,14 +60,9 @@ export class BtnGroup extends Component {
                     }
                 }
             }))
-
-            // let ss=<div>xx</div>
-            // G.d.append(d.query('.btn-group-container'),ss)
-
         }
     }
-
     wrapperInit(para: IBtnGroupPara) {
-        return <div class="btn-group-container" />
+        return <div className="btn-group-container" />
     }
 }
