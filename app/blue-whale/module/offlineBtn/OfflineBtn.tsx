968415b6d9380e2308e63a3cb675330788df2983
翁冰峰 <wengbf@fastlion.cn>
2019-03-06 17:30:09 +0800

    上传成功清空数据

--- app/blue-whale/module/offlineBtn/OfflineBtn.tsx
+++ app/blue-whale/module/offlineBtn/OfflineBtn.tsx
@@ -144,8 +144,9 @@ export class OfflineBtn {
         Shell.imports.uploadcodedata(this.para.uniqueFlag, url, (result) => {
             if (result.success) {
                 Modal.toast('上传成功');
+                this.imports.clear(this.imports.editModule.main);
             } else {
-                Modal.toast(result.msg);
+                Modal.toast(result.msg); 
             }
             loading.destroy();
         });
