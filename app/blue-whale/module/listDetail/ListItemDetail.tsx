3263a9f73f34b81ae54006d0f5c49ecf6710eecc
翁一帆 <wengyf@fastlion.cn>
2019-03-07 17:58:35 +0800

    合并38

--- app/blue-whale/module/listDetail/ListItemDetail.tsx
+++ app/blue-whale/module/listDetail/ListItemDetail.tsx
@@ -14,6 +14,7 @@ import {TextInput} from "../../../global/components/form/text/text";
 import {ContactsModule} from "../flowDesigner/ContactsModule";
 import Component = G.Component;
 import {IDetailBasePara} from "./DetailBase";
+import sys = BW.sys;
 
 export class ListItemDetail extends Component{
     // DOM容器
