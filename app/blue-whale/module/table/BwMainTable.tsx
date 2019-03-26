eb301f1e4d30221665e911082320bc31c6a9e885
翁一帆 <wengyf@fastlion.cn>
2019-03-26 10:47:28 +0800

    表格高度计算

--- app/blue-whale/module/table/BwMainTable.tsx
+++ app/blue-whale/module/table/BwMainTable.tsx
@@ -61,7 +61,7 @@ export class BwMainTableModule extends BwTableModule{
     }
     private tableHeightInit(){
         let ui = this.ui,
-            clientHeight = this.container.clientHeight;
+            clientHeight = this.wrapper.clientHeight;
 
         // 移动端计算表格的高度
         if(tools.isMb) {
@@ -75,7 +75,7 @@ export class BwMainTableModule extends BwTableModule{
             if(ui.reportTitle){
                 clientHeight -= 42;
             }
-            clientHeight -= 45;
+            // clientHeight -= 45;
 
             // iphone x 兼容
             if(tools.cssSupports('height', 'env(safe-area-inset-bottom)')){
