55a10480c6b99fa8cddb5000f3ef3dc9212b1ec5
王加彬 <wangjb@fastlion.cn>
2019-03-29 15:40:45 +0800

    keystep

--- app/blue-whale/module/inputs/inputs.ts
+++ app/blue-whale/module/inputs/inputs.ts
@@ -2,32 +2,32 @@
 import d = G.d;
 import CONF = BW.CONF;
 import tools = G.tools;
-import {Modal} from "../../../global/components/feedback/modal/Modal";
-import {Toast} from "../../../global/components/feedback/toast/Toast";
-import {BwRule} from "../../common/rule/BwRule";
-import {SelectInputMb} from "../../../global/components/form/selectInput/selectInput.mb";
-import {FastBtnTable} from "../../../global/components/FastBtnTable/FastBtnTable";
-import {Button} from "../../../global/components/general/button/Button";
-import {ShellAction} from "../../../global/action/ShellAction";
+import { Modal } from "../../../global/components/feedback/modal/Modal";
+import { Toast } from "../../../global/components/feedback/toast/Toast";
+import { BwRule } from "../../common/rule/BwRule";
+import { SelectInputMb } from "../../../global/components/form/selectInput/selectInput.mb";
+import { FastBtnTable } from "../../../global/components/FastBtnTable/FastBtnTable";
+import { Button } from "../../../global/components/general/button/Button";
+import { ShellAction } from "../../../global/action/ShellAction";
 import Shell = G.Shell;
-import {ListItemDetail} from "../listDetail/ListItemDetail";
+import { ListItemDetail } from "../listDetail/ListItemDetail";
 
 interface InputsPara {
     inputs: R_Input[]
-    locationLine? : string
+    locationLine?: string
     container: HTMLElement
-    keyField? : string
-    table? : Function    // 获取表格ftable
-    afterScan? : Function
-    tableModule? : Function // 获取表格模块
-    queryModule? : Function // 查询器
-    setListItemData? : (data : obj[]) => void // 设置单页数据
+    keyField?: string
+    table?: Function    // 获取表格ftable
+    afterScan?: Function
+    tableModule?: Function // 获取表格模块
+    queryModule?: Function // 查询器
+    setListItemData?: (data: obj[]) => void // 设置单页数据
 }
-interface IKeyStepPara{
-    callback(text : string, input? : R_Input, open? : Function) : Promise<any>
-    container? : HTMLElement
-    inputs? : R_Input[]
-    locationLine? : string
+interface IKeyStepPara {
+    callback(text: string, input?: R_Input, open?: Function): Promise<any>
+    container?: HTMLElement
+    inputs?: R_Input[]
+    locationLine?: string
 }
 /**
  * Inputs在pc端为按键输入，移动端为扫码,根据后台返回类型区分各种操作
@@ -35,13 +35,18 @@ interface IKeyStepPara{
 export class Inputs {
     private p: InputsPara;
     private m: Toast;
-    private isProcess : boolean = false; // 连续扫码，记录当前操作是否结束
+    private isProcess: boolean = false; // 连续扫码，记录当前操作是否结束
     private url: string;  //记录当前请求的url步骤,每次请求从该步骤开始
-    private keyStep : KeyStep;
+    private keyStep: KeyStep;
     constructor(private para: InputsPara) {
         this.p = para;
         para.container.tabIndex = parseInt(G.tools.getGuid());
         this.eventInit(para);
+        // setTimeout(() => {
+        //     this.ajax("http://192.168.1.222:8080/sf/app_sanfu_retail/null/monitorkey/n1-move-19?needparam=%5B%7B%22n%22%3A%22toshopid%22%7D%2C%7B%22n%22%3A%22TOSHOPID%22%7D%5D&toshopid=1111&inputtext=8100247691", () => {
+
+        // })
+        // }, 1000);
     }
 
     /**
@@ -50,7 +55,7 @@ export class Inputs {
      * @param text
      * @param open
      */
-    private matchPass(data: R_Input, text: string, open? : Function) {
+    private matchPass(data: R_Input, text: string, open?: Function) {
         let newUrl = this.url ? this.url : data.dataAddr.dataAddr;
         if (newUrl.indexOf('?') > -1) {
             newUrl += '&';
@@ -61,20 +66,20 @@ export class Inputs {
         return this.ajax(ajaxUrl, open);
     }
 
-    private ajax(aUrl : string, open : Function){
+    private ajax(aUrl: string, open: Function) {
         this.isProcess = true;
         return BwRule.Ajax.fetch(aUrl)
-            .then(({response}) => {
-                this.condition(response,aUrl, open);
+            .then(({ response }) => {
+                this.condition(response, aUrl, open);
                 return response;
             }).catch(() => {
                 this.isProcess = false;
             })
     }
 
-    private condition(response : obj, aUrl : string, open : Function){
+    private condition(response: obj, aUrl: string, open: Function) {
         let elements = response.body && response.body.elements && response.body.elements[0] && response.body.elements[0];
-        if(elements){
+        if (elements) {
             this.atvarParams(elements.atvarparams, elements.subButtons, aUrl, open);
             return;
         }
@@ -84,9 +89,9 @@ export class Inputs {
             atvarObj = body.atvarObj,
             catType = category.type,
             showText = category.showText,
-            ftable : FastBtnTable = tools.isFunction(this.p.table) && this.p.table();
+            ftable: FastBtnTable = tools.isFunction(this.p.table) && this.p.table();
 
-        if(atvarObj){
+        if (atvarObj) {
             this.atvarParams(atvarObj.atvarparams, atvarObj.subButtons, aUrl, open);
         }
 
@@ -120,7 +125,7 @@ export class Inputs {
                 let c = Modal.confirm({
                     msg: showText,
                     btns: ['取消', '确定'],
-                    noHide : true,
+                    noHide: true,
                     callback: (index) => {
                         if (index === true) {
                             this.ajax(CONF.siteUrl + this.url, open);
@@ -141,13 +146,13 @@ export class Inputs {
                 // 提示信息,自动下一步
                 this.ajax(CONF.siteUrl + this.url, open);
                 break;
-            default :
+            default:
                 this.isProcess = false;
                 !atvarObj && this.reOpen(open);
         }
 
         dataType === 0 && this.dataCover(ftable, response);
-        ![2,3].includes(catType) && this.logTip(showText);
+        ![2, 3].includes(catType) && this.logTip(showText);
 
     }
 
@@ -155,7 +160,7 @@ export class Inputs {
      * 再次打开摄像头
      * @param open
      */
-    private reOpen(open : Function){
+    private reOpen(open: Function) {
         typeof open === 'function' && open();
     }
 
@@ -164,20 +169,21 @@ export class Inputs {
      * @param ftable
      * @param response
      */
-    private dataCover(ftable : FastBtnTable, response : obj){
+    private dataCover(ftable: FastBtnTable, response: obj) {
         let data = response.data,
             queryModule = this.para.queryModule && this.para.queryModule();
-
-        if(tools.isEmpty(data)) return;
+        if (tools.isEmpty(data)) return;
 
         queryModule && queryModule.hide();
-        if(queryModule && !ftable){
+        if (queryModule && !ftable) {
             queryModule.para.refresher({}, true).then(() => {
                 this.p.table().data = data;
             })
-        }else if(ftable){
-            ftable.data = data;
-        }else if(tools.isFunction(this.para.setListItemData)){
+        } else if (ftable) {
+            setTimeout(() => {
+                ftable.data = data;
+            }, 1000);
+        } else if (tools.isFunction(this.para.setListItemData)) {
             this.para.setListItemData(data);
         }
     }
@@ -186,50 +192,50 @@ export class Inputs {
      * 提示信息
      * @param showText
      */
-    private logTip(showText : string){
-        if(tools.isEmpty(showText)){
+    private logTip(showText: string) {
+        if (tools.isEmpty(showText)) {
             return;
         }
         this.m && this.m.destroy();
         this.m = new Toast({
             type: 'simple',
-            className : 'max-index',
+            className: 'max-index',
             position: 'bottom',
             content: showText,
             container: this.para.container
         });
     }
 
-    private atvarParams(atvarparams : obj, subButtons : obj[], aUrl : string, open : Function){
+    private atvarParams(atvarparams: obj, subButtons: obj[], aUrl: string, open: Function) {
         this.isProcess = true;
-        let atv, modal =  new Modal({
-            header : {
-                title : '提示'
+        let atv, modal = new Modal({
+            header: {
+                title: '提示'
             },
-            isOnceDestroy : true,
-            isMb : false,
-            top : 50,
-            body : d.create('<div class="inputs-atv"></div>') as HTMLElement,
-            footer : {},
-            onOk : () => {
+            isOnceDestroy: true,
+            isMb: false,
+            top: 50,
+            body: d.create('<div class="inputs-atv"></div>') as HTMLElement,
+            footer: {},
+            onOk: () => {
                 let atvData = atv.dataGet(),
-                    url = tools.url.addObj(CONF.siteUrl + subButtons[0].actionAddr.dataAddr, atvData ? {'atvarparams': JSON.stringify(atv.dataGet())} : null);
+                    url = tools.url.addObj(CONF.siteUrl + subButtons[0].actionAddr.dataAddr, atvData ? { 'atvarparams': JSON.stringify(atv.dataGet()) } : null);
 
                 //必选判断
                 let errTip = '';
                 atvarparams.forEach(obj => {
-                    if(obj.atrrs.requiredFlag === 1 && atvData[obj.field_name] === ''){
+                    if (obj.atrrs.requiredFlag === 1 && atvData[obj.field_name] === '') {
                         errTip += obj.caption + ',';
                     }
                 });
-                if(errTip !== ''){
-                    Modal.alert(errTip.substring(0,errTip.length - 1) + '不能为空');
+                if (errTip !== '') {
+                    Modal.alert(errTip.substring(0, errTip.length - 1) + '不能为空');
                     return;
                 }
                 modal.destroy();
-                BwRule.Ajax.fetch(url,{
-                    type : 'get',
-                }).then(({response}) => {
+                BwRule.Ajax.fetch(url, {
+                    type: 'get',
+                }).then(({ response }) => {
                     this.condition(response, aUrl, open);
                 }).catch(() => {
                     this.isProcess = false;
@@ -263,16 +269,16 @@ export class Inputs {
      * @param para
      */
     private eventInit(para: InputsPara) {
-        if(G.tools.isMb){
+        if (G.tools.isMb) {
             this.keyStep = new KeyStep({
-                inputs : para.inputs,
-                callback : (ajaxData, input, open) => {
-                    if(input && !this.isProcess){
+                inputs: para.inputs,
+                callback: (ajaxData, input, open) => {
+                    if (input && !this.isProcess) {
                         return this.matchPass(input, ajaxData, open);
-                    }else if(para.locationLine){
+                    } else if (para.locationLine) {
                         return this.rowSelect(para.locationLine, ajaxData);
-                    }else {
-                        return new Promise(resolve => {resolve()})
+                    } else {
+                        return new Promise(resolve => { resolve() })
                     }
                 }
             });
@@ -284,22 +290,22 @@ export class Inputs {
                 line = para.locationLine;
             d.on(para.container, 'keydown', (e: KeyboardEvent) => {
                 let handle = () => {
-                        let reg = regExpMatch(input, text);
-                        //匹配成功
-                        if(reg){
-                            this.matchPass(reg, text);
-                        }else if(line){
-                            this.rowSelect(line, text);
-                        }
+                    let reg = regExpMatch(input, text);
+                    //匹配成功
+                    if (reg) {
+                        this.matchPass(reg, text);
+                    } else if (line) {
+                        this.rowSelect(line, text);
+                    }
 
-                        timer = null;
-                        text = '';
-                    },
+                    timer = null;
+                    text = '';
+                },
                     code = e.keyCode || e.which || e.charCode;
 
-                if(code === 13){
+                if (code === 13) {
                     handle();
-                }else {
+                } else {
                     text += e.key;
                     if (timer) {
                         clearTimeout(timer);
@@ -311,13 +317,13 @@ export class Inputs {
         });
     }
 
-    private rowSelect(line : string, text : string){
+    private rowSelect(line: string, text: string) {
         return new Promise(resolve => {
             let fstable = tools.isFunction(this.para.table) && this.para.table(),
                 index = fstable && fstable.locateToRow(line, text, true),
                 tableModule = tools.isFunction(this.para.tableModule) && this.para.tableModule();
 
-            if(tools.isNotEmpty(index) && tableModule && tableModule.bwEl.subTableList) {
+            if (tools.isNotEmpty(index) && tableModule && tableModule.bwEl.subTableList) {
                 tableModule.subRefreshByIndex(index);
             }
             resolve();
@@ -328,30 +334,30 @@ export class Inputs {
 /**
  *
  */
-export class KeyStep{
-    private p : IKeyStepPara;
-    constructor(para : IKeyStepPara){
+export class KeyStep {
+    private p: IKeyStepPara;
+    constructor(para: IKeyStepPara) {
         this.p = para;
         let can2dScan = Shell.inventory.can2dScan,
-            btn : Button;
+            btn: Button;
 
-        if(!can2dScan && tools.isMb){
+        if (!can2dScan && tools.isMb) {
             btn = new Button({
                 type: 'link',
                 content: '扫码查询',
                 size: 'large',
                 container: para.container,
                 className: 'keystep',
-                icon:  'richscan_icon',
+                icon: 'richscan_icon',
                 onClick: () => {
                     this.evenHandle(para, can2dScan);
                 }
             });
-        }else {
+        } else {
             this.evenHandle(para, can2dScan);
         }
         // 拖动
-        d.on(btn.wrapper, 'touchstart', function (e : TouchEvent) {
+        d.on(btn.wrapper, 'touchstart', function (e: TouchEvent) {
             let ev = e.touches[0],
                 wrapper = btn.wrapper,
                 evX = ev.clientX,
@@ -359,7 +365,7 @@ export class KeyStep{
                 top = wrapper.offsetTop,
                 left = wrapper.offsetLeft;
 
-            let moveHandler = function (i : TouchEvent) {
+            let moveHandler = function (i: TouchEvent) {
                 let iv = i.touches[0],
                     ivX = iv.clientX,
                     ivY = iv.clientY,
@@ -369,36 +375,36 @@ export class KeyStep{
                 wrapper.style.top = top - distanceY + 'px';
             };
 
-            let endHandler =  function () {
+            let endHandler = function () {
                 d.off(document, 'touchmove', moveHandler);
                 d.off(document, 'touchend', endHandler);
             };
 
-            d.on(document, 'touchmove',moveHandler);
-            d.on(document, 'touchend',endHandler);
+            d.on(document, 'touchmove', moveHandler);
+            d.on(document, 'touchend', endHandler);
         })
     }
 
-    evenHandle(para : IKeyStepPara, can2dScan : boolean){
-        if(Array.isArray(para.inputs)){
+    evenHandle(para: IKeyStepPara, can2dScan: boolean) {
+        if (Array.isArray(para.inputs)) {
             para.inputs.forEach(input => {
                 let type = input.inputType;
                 switch (type) {
                     case '0':
                     case '2': // 移动端支持连续扫码
-                        if(can2dScan){
+                        if (can2dScan) {
                             let open = () => {
-                                this.scanOpen().then((res : obj) => {
-                                    if(res.success && res.data !== 'openSuponScan') {
+                                this.scanOpen().then((res: obj) => {
+                                    if (res.success && res.data !== 'openSuponScan') {
                                         this.p.callback(res.data, regExpMatch(input, res.data))
                                     }
                                     open();
                                 });
                             };
                             open();
-                        }else {
+                        } else {
                             let open = () => {
-                                this.open(para).then((text : string) => {
+                                this.open(para).then((text: string) => {
                                     let reg = regExpMatch(input, text),
                                         conScan = tools.isMb && type === '2';
 
@@ -413,25 +419,25 @@ export class KeyStep{
                         }
                         break;
                     case '1':
-                        if(can2dScan){
+                        if (can2dScan) {
                             this.startEpc(input);
                         }
                         break;
                 }
             })
-        }else if(para.locationLine){ // 定位功能
-            if(can2dScan){
+        } else if (para.locationLine) { // 定位功能
+            if (can2dScan) {
                 let open = () => {
-                    this.scanOpen().then((res : obj) => {
-                        if(res.success && res.data !== 'openSuponScan') {
+                    this.scanOpen().then((res: obj) => {
+                        if (res.success && res.data !== 'openSuponScan') {
                             this.p.callback(res.data).then()
                         }
                         open();
                     });
                 };
                 open();
-            }else {
-                this.open(para).then((text : string) => {
+            } else {
+                this.open(para).then((text: string) => {
                     this.p.callback(text).then()
                 });
 
@@ -439,7 +445,7 @@ export class KeyStep{
         }
     }
 
-    scanOpen(){
+    scanOpen() {
         return new Promise((resolve) => {
             Shell.inventory.scan2dOn((res) => {
                 resolve(res)
@@ -452,7 +458,7 @@ export class KeyStep{
      * type0 rfid及手机扫码
      * @param {IKeyStepPara} para
      */
-    open(para : IKeyStepPara){
+    open(para: IKeyStepPara) {
         return new Promise((resolve) => {
             (ShellAction.get().device().scan as any)({
                 callback: (even) => {
@@ -462,25 +468,25 @@ export class KeyStep{
         })
     }
 
-    startEpc(input : R_Input){
+    startEpc(input: R_Input) {
         let timer = null, isLoading = false, arr = [];
         Shell.inventory.startEpc(null, (result) => {
-            if(result.success){
+            if (result.success) {
                 result.data.forEach(obj => {
                     let reg = regExpMatch(input, obj.epc);
                     reg && arr.push(obj.epc);
                 });
 
-                if(!timer && !isLoading) {
+                if (!timer && !isLoading) {
                     timer = setTimeout(() => {
                         isLoading = true;
-                        if(tools.isNotEmpty(arr)){
+                        if (tools.isNotEmpty(arr)) {
                             this.p.callback(arr.join(','), input)
                                 .then(() => isLoading = false);
                         }
                         arr = [];
                         timer = null;
-                    },1000);
+                    }, 1000);
                 }
             }
         });
@@ -492,7 +498,7 @@ export class KeyStep{
  * @param input
  * @param inputContent
  */
-function regExpMatch(input : R_Input, inputContent: string) {
+function regExpMatch(input: R_Input, inputContent: string) {
     let regArr = null,
         data = null,
         len = inputContent.length,
