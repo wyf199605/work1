de1836b7fe3848df4c10d4c8e65e767677c007b4
翁一帆 <wengyf@fastlion.cn>
2019-04-02 18:02:49 +0800

    主从表判断改为捕获阶段

--- app/blue-whale/module/table/newTableModule.tsx
+++ app/blue-whale/module/table/newTableModule.tsx
@@ -887,19 +887,29 @@ export class NewTableModule extends AGroupTabItem{
             };
 
         let on = () => {
-            this.sub && d.on(this.subWrapper, 'click', handler1 = () => {
+            /*this.sub && d.on(this.subWrapper, 'click', handler1 = () => {
                 isMainActive = false;
                 tools.isFunction(onChange) && onChange(isMainActive);
             });
             d.on(this.main.wrapper, 'click', handler2 = () => {
                 isMainActive = true;
                 tools.isFunction(onChange) && onChange(isMainActive);
-            });
+            });*/
+            this.sub && this.subWrapper.addEventListener('click', handler1 = () => {
+                isMainActive = false;
+                tools.isFunction(onChange) && onChange(isMainActive);
+            }, true);
+            this.main.wrapper.addEventListener('click', handler2 = () => {
+                isMainActive = true;
+                tools.isFunction(onChange) && onChange(isMainActive);
+            }, true)
         };
 
         let off = () => {
-            this.sub && d.off(this.subWrapper, 'click', handler1);
-            d.off(this.main.wrapper, 'click', handler2);
+            // this.sub && d.off(this.subWrapper, 'click', handler1);
+            // d.off(this.main.wrapper, 'click', handler2);
+            this.sub && this.subWrapper.removeEventListener('click', handler1, true);
+            this.main.wrapper.removeEventListener('click', handler2, true);
         };
 
         return {
