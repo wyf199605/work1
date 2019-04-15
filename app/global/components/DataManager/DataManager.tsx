b9db9565e75e6b74587f2c4b96ebfb32ae34ce0d
翁一帆 <wengyf@fastlion.cn>
2019-04-15 10:45:51 +0800

    数据展示出错

--- app/global/components/DataManager/DataManager.tsx
+++ app/global/components/DataManager/DataManager.tsx
@@ -110,7 +110,8 @@ export class DataManager {
                     .then(({data, total}) => {
                         if(once){
                             this.allData = data;
-                            data = this.allData.slice(0, this.pageSize);
+                            if(tools.isMb)
+                                data = this.allData.slice(0, this.pageSize > 0 ? this.pageSize : data.length);
                         }
                         if (isSetData) {
                             this.data = data;
@@ -120,7 +121,8 @@ export class DataManager {
                         this._total = total;
                         resolve(total);
                     })
-                    .catch(() => {
+                    .catch((e) => {
+                        console.log(e);
                         reject();
                     })
                     .finally(() => {
