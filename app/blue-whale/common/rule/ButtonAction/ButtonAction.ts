06e28f6fbb25bcd026830b0f4505238bfe1eefe7
gaosen <gaoqs@fastlion.cn>
2019-04-16 09:20:17 +0800

    处理出差打卡需求

--- app/blue-whale/common/rule/ButtonAction/ButtonAction.ts
+++ app/blue-whale/common/rule/ButtonAction/ButtonAction.ts
@@ -625,16 +625,30 @@ export class ButtonAction {
                             data[1] = res;
                             modal.destroy();
                         }
-
+                        console.log('-------------------------');
+                        // console.log(table.tableModule.main.ftable)
                         if(obj.openType === 'buildmap') {
-                            console.log(obj);
+                            
                             // BwRule.reqAddr(obj.actionAddr)
-                             const url = CONF.siteUrl + BwRule.reqAddr(obj.actionAddr)
-                            return BwRule.Ajax.fetch(url ).then(({ response }) => {
-                                console.log('localSubmit');
-                                G.Shell.location.localSubmit(response);
-                            });
+                            //  let url = CONF.siteUrl + BwRule.reqAddr(obj.actionAddr);
+                            //     url = `${url}&startTime${res.atvarparams[0].atrrs.defaultValue}
+                            //         &endTime${res.atvarparams[0].atrrs.defaultValue}`;
+                            // return BwRule.Ajax.fetch(url ).then(({ response }) => {
+                            //     console.log('localSubmit');
+                            //     const localSubmitObj = {
+                            //         startTime: res.atvarparams[0].atrrs.defaultValue,
+                            //         endTime: res.atvarparams[0].atrrs.defaultValue,
+                            //         dataAddr: obj.actionAddr,
+                            //     }
+                            //     G.Shell.location.localSubmit(localSubmitObj);
+                            // });
                             //  G.Shell.location.localSubmit(response)
+                            const localSubmitObj = {
+                                        startTime: res.atvarparams[0].atrrs.defaultValue,
+                                        endTime: res.atvarparams[0].atrrs.defaultValue,
+                                        dataAddr: obj.actionAddr.dataAddr,
+                                    }
+                            return G.Shell.location.localSubmit(localSubmitObj);
                         }
                         this.clickHandle(obj, data, (r) => {
                             onOk();
