1da4432b8f36848b20fff0ffa9f3ed80041c1032
王加彬 <wangjb@fastlion.cn>
2019-04-22 19:02:06 +0800

    设备号

--- app/blue-whale/module/unBinding/UnBinding.tsx
+++ app/blue-whale/module/unBinding/UnBinding.tsx
@@ -1,79 +1,88 @@
 /// <amd-module name="UnBinding"/>
 
-import {Modal} from "../../../global/components/feedback/modal/Modal";
-import {BwRule} from "../../common/rule/BwRule";
+import { Modal } from "../../../global/components/feedback/modal/Modal";
+import { BwRule } from "../../common/rule/BwRule";
 import sys = BW.sys;
 import tools = G.tools;
 import CONF = BW.CONF;
 import d = G.d;
 interface UnBindingPara {
-    MODEL : string
-    VENDOR : string
-    REGISTER_TIME : string
-    UUID : string
+    MODEL: string
+    VENDOR: string
+    REGISTER_TIME: string
+    UUID: string
 }
 export class UnBinding {
-    private modal:Modal;
-    constructor(data : UnBindingPara[]){
+    private modal: Modal;
+    constructor(data: UnBindingPara[]) {
         let full;
-        if(sys.os !== 'pc') {
+        if (sys.os !== 'pc') {
             full = 'full';
         }
-        let ul = <ul className="device-view"/>;
+        let ul = <ul className="device-view" />;
         this.modal = new Modal({
-            header : '设备管理',
-            body : ul,
-            position : full,
-            isOnceDestroy : true
+            header: '设备管理',
+            body: ul,
+            position: full,
+            isOnceDestroy: true
         });
         this.modal.isShow = true;
-
+        let deviceInfo = JSON.parse(localStorage.getItem("deviceInfo"))
         //遍历li
-        if(data.length>0){
-            let first=<div class="dev_title"><p>当前设备</p></div>
-            ul.appendChild(first)
-            first.appendChild(this.deviceTpl(data[0]));
-        }
+        if (data.length > 0) {
+            let list = data.filter(item => {
+                return item.UUID == deviceInfo.uuid
+            })
+            if (list.length > 0) {
+                let first = <div class="dev_title"><p>当前设备</p></div>
+                ul.appendChild(first)
+                first.appendChild(this.deviceTpl(list[0]));
+            }
 
-        if(data.length>1){
-            let second=<div class="dev_title"><p>其他设备</p></div>
-            ul.appendChild(second)
-            let list=data.slice(1)
-            list.forEach(d => {
-                ul.appendChild(this.deviceTpl(d));
-            });
+        }
+        if (data.length > 1) {
+            let list = data.filter(item => {
+                return item.UUID != deviceInfo.uuid
+            })
+            if (list.length > 0) {
+                let second = <div class="dev_title"><p>其他设备</p></div>
+                ul.appendChild(second)
+                list.forEach(d => {
+                    ul.appendChild(this.deviceTpl(d));
+                });
+            }
         }
         let self = this;
-            //unbind
+        //unbind
         d.on(ul, 'click', '.unbind', function () {
-                let data = JSON.stringify([{uuid : this.dataset.name}]);
-                // Rule.ajax(CONF.ajaxUrl.unbound,{
-                //     type: 'post',
-                //     data: data,
-                //     success : function (r) {
-                //         self.modal.isShow = false;
-                //         Modal.toast('解绑成功');
-                //     }
-                // });
-                BwRule.Ajax.fetch(CONF.ajaxUrl.unbound, {
-                    type: 'post',
-                    data: data,
-                }).then(() => {
-                    self.modal.isShow = false;
-                    Modal.toast('解绑成功');
-                });
+            let data = JSON.stringify([{ uuid: this.dataset.name }]);
+            // Rule.ajax(CONF.ajaxUrl.unbound,{
+            //     type: 'post',
+            //     data: data,
+            //     success : function (r) {
+            //         self.modal.isShow = false;
+            //         Modal.toast('解绑成功');
+            //     }
+            // });
+            BwRule.Ajax.fetch(CONF.ajaxUrl.unbound, {
+                type: 'post',
+                data: data,
+            }).then(() => {
+                self.modal.isShow = false;
+                Modal.toast('解绑成功');
             });
+        });
     }
 
-    show(){
+    show() {
         this.modal.isShow = true;
     }
 
-    private deviceTpl(data : UnBindingPara) : HTMLUListElement {
+    private deviceTpl(data: UnBindingPara): HTMLUListElement {
         return <li className="device-cell">
             <div className="icon-box">
 
-                <span className={`iconfont ${tools.isMb ? 'icon-device-mb' : 'icon-device-pc'}`}/>
+                <span className={`iconfont ${tools.isMb ? 'icon-device-mb' : 'icon-device-pc'}`} />
             </div>
             <div className="device-name">
                 <div className="device-modal">型号：{data.MODEL}</div>
