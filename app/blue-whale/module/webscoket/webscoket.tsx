d6d57f25edd74731005e119553f1f3c0082d5277
linh <linh@fastlion.cn>
2019-05-28 17:44:02 +0800

    消息角标无法持续显示bug修复，建议后期修改为同步请求

--- app/blue-whale/module/webscoket/webscoket.tsx
+++ app/blue-whale/module/webscoket/webscoket.tsx
@@ -77,7 +77,13 @@ export = class webscoket {
             heartCheck.reset().start();
             console.info("websocket 连接打开.");
             require(['messagePage'], (messagePage) => {
-                messagePage.setSysBadge();
+                // 统计消息数量
+                BwRule.Ajax.fetch(CONF.url.taskMsg, {
+                    dataType: 'json',
+                    type: 'get'
+                }).then(({ response }) => {
+                    messagePage.setSysBadge(response.data.length);
+                });
             });
 
         };
