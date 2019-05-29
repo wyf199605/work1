66b45338c6f6c765e0e0cdb21a579041c7325718
gaosen <gaoqs@fastlion.cn>
2019-05-29 14:56:02 +0800

    二维码分享细节修改

--- app/blue-whale/common/share-code/shareCode.tsx
+++ app/blue-whale/common/share-code/shareCode.tsx
@@ -345,10 +345,7 @@ export class ShareCode {
         </div>
 
         shareEle.addEventListener('click', (e: Event) => {
-            let sharePage: HTMLDivElement = <div class="share-page">
-                {/* <div class="share-page-qrcode"></div> */}
-            </div>
-            d.query('body').appendChild(sharePage);
+            
             switch (e.target['className']) {
                 case 'qr-code-share':
                     this.generateCode();
@@ -356,6 +353,9 @@ export class ShareCode {
                     break;
                 case 'fastlion-share':
                     d.query('body').removeChild(shareEle);
+                    let sharePage: HTMLDivElement = <div class="share-page"></div>
+                    d.query('body').appendChild(sharePage);
+                    
                     let timer = setTimeout(() => {
                         clearTimeout(timer);
                         tools.isMb && Shell.image.getSignImg((res) => {
@@ -398,7 +398,7 @@ export class ShareCode {
                     break;
                 case 'cancel-share':
                 default:
-                    d.query('body').removeChild(shareEle);
+                    document.querySelector('body').removeChild(shareEle);
                     break;
             }
         })
