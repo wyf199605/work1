3e33f5077c18c3a6fe2c08fff747be8d678a5137
gaosen <gaoqs@fastlion.cn>
2019-06-11 15:15:43 +0800

    处理drill类型

--- app/blue-whale/module/echart-module/chartTableModule.tsx
+++ app/blue-whale/module/echart-module/chartTableModule.tsx
@@ -52,19 +52,17 @@ export class ChartTableModule {
         this.parentDom.insertBefore(this.render(), this.parentDom.childNodes[0]);
         // this.parentDom.appendChild(this.render());
         let chart;
+        debugger;
         switch (this.ui.uiType) {
             case 'select':
             case 'table':
-                    // this.initPieChartFn();
+            case 'web':
                 chart = this.ui.showType === 'pie'? this.initPieChartFn(): this.initCommonChartFn();
-                
                 break;
             case 'detail':
                 chart = this.ui.fields ? null : this.ui.showType === 'pie'? this.initPieChartFn(): this.initCommonChartFn();
                 break;
         }
-
-        // let line = this.lineChartFn();
         window.onresize = () => {
             chart && chart.resize();
         }
@@ -310,105 +308,5 @@ export class ChartTableModule {
             }
         })
     }
-    /**
-     * 测试表格
-     */
-    lineChartFn() {
-
-        let lineChart = echarts.init(this.chartDom);
-        const lineChartData = {
-            title: {
-                text: '本月经营分析',
-                textStyle: {
-                    fontFamily: 'monospace',
-                    fontSize: 18,
-                    color: '#333'
-                    // fontWeight: 'bold',
-
-                },
-                padding: 15,
-            },
-            grid: {
-                // top: 15,
-                left: 15,
-                right: 15,
-                bottom: 15,
-                containLabel: true
-            },
-            tooltip: {},
-            legend: {
-                data: ['销量', '购买力'],
-                top: 15,
-            },
-            xAxis: {
-                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-                splitLine: {
-                    lineStyle: {
-                        // 使用深浅的间隔色
-                        color: '#f7f7f8'
-                    },
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: '#d5d5d5'
-                    }
-                },
-                axisLabel: {
-                    color: '#333333'
-                }
-            },
-
-            yAxis: {
-                splitLine: {
-                    lineStyle: {
-                        // 使用深浅的间隔色
-                        color: '#f7f7f8'
-                    },
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: '#d5d5d5'
-                    }
-                },
-                axisLabel: {
-                    color: '#333333'
-                }
-            },
-            series: [
-                {
-                    name: '销量',
-                    type: 'line',
-                    color: '#609ee9',
-                    lineMaxWidth: '10',
-                    smooth: true,
-                    symbol: 'circle',
-                    symbolSize: 8,
-                    data: [5, 20, 36, 10, 10, 20]
-                },
-                {
-                    name: '购买力',
-                    type: 'line',
-                    color: '#f7ba2a',
-                    smooth: true,
-                    lineMaxWidth: '10',
-                    symbol: 'circle',
-                    symbolSize: 8,
-                    data: [7, 18, 46, 52, 60, 16]
-                },
-                {
-                    name: '进货量',
-                    type: 'line',
-                    smooth: true,
-                    color: '#39ca74',
-                    lineMaxWidth: '10',
-                    symbol: 'circle',
-                    symbolSize: 8,
-                    data: [55, 24, 36, 33, 42, 28]
-                },
-            ]
-        };
-        lineChart.setOption(lineChartData);
-        return lineChart;
-
-    }
+    
 }
